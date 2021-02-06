ed592bec420ed30efe5ec02429fae14dace1edc9
Eric S. Londres <elondres@stevens.edu>
2021-02-05 19:45:45 -0500

    implement simple call-and-response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting BeerHolderBot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |message| async move {
+        message.answer("Hello, world!").send().await?;
+        ResponseResult::<()>::Ok(())
+    })
+    .await;
 }
